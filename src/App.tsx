d13d2fd08bd51cd191be2d823b659ca8fc084ade
Bryann Henrique <bryann.enrique@gmail.com>
2023-02-21 16:15:45 -0300

    Configurações iniciais finalizadas.

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,102 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useCallback } from 'react';
+import ReactFlow, {
+  addEdge,
+  Background,
+  Connection,
+  ConnectionMode,
+  Controls,
+  Node,
+  useEdgesState,
+  useNodesState
+} from 'reactflow';
+import 'reactflow/dist/style.css';
+import * as Toolbar from '@radix-ui/react-toolbar';
+import DefaultEdge from './edges/DefaultEdge';
+import { zinc } from 'tailwindcss/colors';
 
-function App() {
-  const [count, setCount] = useState(0)
+import { Square } from './nodes/Square';
+import { Ellipse } from './nodes/Ellipse';
+
+const NODE_TYPES = {
+  square: Square,
+  ellipse: Ellipse,
+}
+
+const EDGE_TYPES = {
+  default: DefaultEdge,
+}
+
+const INITIAL_NODES = [
+  {
+    id: crypto.randomUUID(),
+    type: 'square',
+    position: { x: 200, y: 400 },
+    data: {}
+  },
+  {
+    id: crypto.randomUUID(),
+    type: 'ellipse',
+    position: { x: 600, y: 400 },
+    data: {}
+  }
+] satisfies Node[];
+
+export const App = () => {
+  const [edges, setEdges, onEdgesChange] = useEdgesState([]);
+  const [nodes, setNodes, onNodesChange] = useNodesState(INITIAL_NODES);
+
+  const onConnect = useCallback((connection: Connection) => {
+    return setEdges(edges => addEdge(connection, edges));
+  }, []);
+
+  function addSquareNode(type: keyof typeof NODE_TYPES) {
+    setNodes(nodes => [
+      ...nodes,
+      {
+        id: crypto.randomUUID(),
+        type,
+        position: { x: 750, y: 350 },
+        data: {}
+      }
+    ]);
+  }
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className='w-screen h-screen'>
+      <ReactFlow
+        nodeTypes={NODE_TYPES}
+        defaultEdgeOptions={{
+          type: 'default'
+        }}
+        edgeTypes={EDGE_TYPES}
+        edges={edges}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        nodes={nodes}
+        onNodesChange={onNodesChange}
+        connectionMode={ConnectionMode.Loose}
+      >
+        <Background
+          gap={12}
+          size={1}
+          color={zinc[400]}
+        />
+
+        <Controls />
+      </ReactFlow>
+
+
+      <Toolbar.Root className='fixed flex gap-3 bottom-20 left-1/2 -translate-x-1/2 bg-white rounded-2xl shadow-lg border border-zinc-300 px-8 h-20 w-96 overflow-hidden'>
+        <Toolbar.Button
+          onClick={() => addSquareNode('square')}
+          className='w-24 h-24 bg-violet-500 mt-6 rounded hover:-translate-y-2 transition-transform'
+        />
+
+        <Toolbar.Button
+          onClick={() => addSquareNode('ellipse')}
+          className='w-24 h-24 bg-violet-500 mt-6 rounded-2/1 hover:-translate-y-2 transition-transform'
+        />
+      </Toolbar.Root>
     </div>
   )
 }
-
-export default App
---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  important: true,
+  content: ['./src/**/*.tsx'],
+  theme: {
+    borderRadius: {
+      '2/1': '50%'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
